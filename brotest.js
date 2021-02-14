c8b294970813b5217b19e72a17563189fcccc3cb
Kipre <kipr@live.ru>
2021-02-14 20:58:48 +0100

    wip and basics

--- brotest.js
+++ brotest.js
@@ -1,7 +1,52 @@
+class Bro {
+    constructor() {
+        this.tests = [];
+    }
+    
+    test(name, fn, timeout) {
+        this.tests.push({name, fn, timeout});
+    }
 
-export class test {
-    constructor(comment, callback) {
-        this.comment = comment;
-        this.callback = callback;
+    expect(value) {
+        return new Expectation(value);
+    }
+
+    run() {
+        let errors = 0;
+        for (const {name, fn, timeout} of this.tests) {
+            try {
+                fn();
+            } catch (e) {
+                console.error(name, 'failed', e);
+                errors++;
+            }
+        }
+        if (!errors) {
+            console.log(`%cAll ${this.tests.length} tests ran successfully.`, 'color: green');
+        } else {
+            console.log(`%c${errors} out of ${this.tests.length} tests failed.`, 'color: red');
+        }
     }
 }
+
+class Expectation {
+    constructor(value) {
+        this.value = value;
+    }
+
+    toBe(expectedValue) {
+        if (this.value !== expectedValue) {
+            throw `because ${this.value} is not ${expectedValue}`;
+        }
+    }
+
+    toMatchObject(object) {
+        for (const property in object) {
+            if (this.value[property] != object[property]) {
+                throw `because in property "${property}" that is ${this.value[property]} does not match ${object[property]}`;
+            }
+        }
+    }
+}
+
+export default new Bro();
